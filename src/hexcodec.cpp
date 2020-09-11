4c96fe4a45ed071c2011bcc25ef0f03f84d73f5c
Dmitrii Ivanov <idma88@yandex.ru>
2020-09-11 23:22:17 +0300

    Добавлен класс для преобразования строк между "сырым" (ASCII) и шестнадцатеричным представлениями

---
+++ src/hexcodec.cpp
@@ -0,0 +1,56 @@
+﻿/*
+ * This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+ * PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+ */
+#include "hexcodec.h"
+
+namespace SCS
+{
+    HexCodec::HexCodec() : m_value("")
+    {
+    }
+
+    HexCodec::HexCodec(const std::string& value, bool isHex /*= false*/)
+    {
+        if (isHex)
+            SetHex(value);
+        else
+            SetRaw(value);
+    }
+
+    std::string HexCodec::GetRaw()
+    {
+        return m_value;
+    }
+
+    void HexCodec::SetRaw(const std::string& value)
+    {
+        m_value = value;
+    }
+
+    std::string HexCodec::GetHex()
+    {
+        return boost::algorithm::hex(m_value);
+    }
+
+    void HexCodec::SetHex(const std::string& value)
+    {
+        try
+        {
+            m_value = boost::algorithm::unhex(value);
+        }
+        catch (const boost::algorithm::not_enough_input&)
+        {
+            SetErrorMessage("Invalid hex sequence: input does not contain enough hex digits");
+        }
+        catch (const boost::algorithm::non_hex_input&)
+        {
+            SetErrorMessage("Invalid hex sequence: input contains non-hexadecimal characters");
+        }
+    }
+
+    HexCodec::operator std::string()
+    {
+        return GetRaw();
+    }
+} // namespace SCS
---
+++ src/hexcodec.h
@@ -0,0 +1,70 @@
+﻿/**
+ * Класс для преобразования строк между "сырым" (ASCII) и шестнадцатеричным представлениями
+ */
+
+#ifndef _SCS_HEXCODEC_H
+#define _SCS_HEXCODEC_H
+
+#include <string>
+
+#include <boost/algorithm/hex.hpp>
+
+#include "ErrorMessage.h"
+
+namespace SCS
+{
+    class HexCodec : public ErrorMessage
+    {
+      public:
+        /**
+         * Конструктор по-умолчанию
+         */
+        HexCodec();
+
+        /**
+         * Конструктор
+         * @param value - [in] Инициализируемое значение
+         * @param isHex - [in] Признак, шеснадцатиричного представления значения
+         */
+        HexCodec(const std::string& value, bool isHex = false);
+
+        /**
+         * Деструктор
+         */
+        ~HexCodec() = default;
+
+        /**
+         * Получить значение в "сыром" (ASCII) представлении
+         * @returns Возвращает значение в "сыром" (ASCII) представлении
+         */
+        std::string GetRaw();
+
+        /**
+         * Установить значение в "сыром" (ASCII) представлении
+         * @param value - [in] Новое значение
+         */
+        void SetRaw(const std::string& value);
+
+        /**
+         * Получить значение в шеснадцатиричном представлении
+         * @returns Возвращает значение в шеснадцатиричном представлении
+         */
+        std::string GetHex();
+
+        /**
+         * Установить значение в шеснадцатиричном представлении
+         * @param value - [in] Новое значение
+         */
+        void SetHex(const std::string& value);
+
+        /**
+         * Оператор преобразования в std::string
+         * @returns Возвращает значение в "сыром" (ASCII) представлении
+         */
+        operator std::string();
+
+      private:
+        std::string m_value;
+    };
+} // namespace SCS
+#endif // !_SCS_HEXCODEC_H
