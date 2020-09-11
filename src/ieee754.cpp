27fb971c1562f7eee30b3585105e9c29e4cdd7ab
Dmitrii Ivanov <idma88@yandex.ru>
2020-09-11 23:23:52 +0300

    Добавлен класс для работы с рациональными числами, в представлении, описанном стандартом IEEE754

---
+++ src/ieee754.cpp
@@ -0,0 +1,40 @@
+﻿/*
+ * This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+ * PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+ */
+#include "ieee754.h"
+
+namespace SCS
+{
+    bool IEEE754::IsIEEE754(const std::string& value)
+    {
+        if (value.at(0) != IEEE754_PREFFIX) return false;
+        if (value.length() != 9) return false;
+
+        return std::all_of(value.begin() + 1, value.end(), [](char c) {
+            return (c >= '0') & (c <= '9') | (c >= 'a') & (c <= 'f') | (c >= 'A') & (c <= 'F');
+        });
+    }
+
+    std::string IEEE754::ToString(const float value)
+    {
+        std::stringstream stream;
+
+        stream << std::setfill('0') << std::setw(8) << std::hex << *reinterpret_cast<int*>(const_cast<float*>(&value));
+        return IEEE754_PREFFIX + stream.str();
+    }
+
+    float IEEE754::ToFloat(const std::string& value)
+    {
+        std::string       str = value;
+        std::stringstream stream;
+        int               resValue;
+
+        if (str.at(0) == IEEE754_PREFFIX) str = str.substr(1, str.length() - 1);
+
+        stream << std::hex << str;
+        stream >> resValue;
+
+        return *reinterpret_cast<float*>(&resValue);
+    }
+} // namespace SCS
---
+++ src/ieee754.h
@@ -0,0 +1,44 @@
+﻿/**
+ * Класс для работы с рациональными числами, в представлении, описанном стандартом IEEE754
+ */
+
+#ifndef _SCS_IEEE754_H
+#define _SCS_IEEE754_H
+
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+namespace SCS
+{
+    class IEEE754
+    {
+      public:
+        /**
+         * Определяет, является ли указанная строка рациональным числом в IEEE754 представлении
+         * @param value - [in] Строка для анализа
+         * @return Возвращает true, если строка похожа на IEEE754 представление или false в противном случае
+         */
+        static bool IsIEEE754(const std::string& value);
+
+        /**
+         * Преобразует число из float в строку в IEEE754 представлении
+         * @param value - [in] Число для преобразования
+         * @return Возвращает строку в IEEE754 представлении указанного числа
+         */
+        static std::string ToString(const float value);
+
+        /**
+         * Преобразует строку в IEEE754 представлении в число
+         * @param value - [in] Cтрока в IEEE754 представлении
+         * @return Возвращает число указанного строкового IEEE754 представления
+         */
+        static float ToFloat(const std::string& value);
+
+      private:
+        // Префик для чисел в IEEE754 нотации
+        static const char IEEE754_PREFFIX = '&';
+    };
+} // namespace SCS
+#endif // !_SCS_IEEE754_H
