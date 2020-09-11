10bc96e3479e3c427e4205e317898511d9bc1fba
Dmitrii Ivanov <idma88@yandex.ru>
2020-09-11 23:20:47 +0300

    Добавлены вспомогательные алгоритмы

---
+++ src/algoritms.h
@@ -0,0 +1,23 @@
+﻿/**
+ * Различные вспомогательные алгоритмы
+ */
+
+#ifndef _SCS_ALGORITMS_H
+#define _SCS_ALGORITMS_H
+
+namespace SCS
+{
+    /**
+     * Возвращает значение ограниченное заданым диапазоном
+     * @param value - [in] Проверяемое значение
+     * @param low   - [in] Нижняя граница
+     * @param high  - [in] Верхняя граница
+     * @return Возвращает value, ограниченное диапазоном от low до high включительно
+     */
+    template <class T>
+    constexpr const T& clamp(const T& value, const T& low, const T& high)
+    {
+        return (value < low ? low : (value > high ? high : value));
+    }
+} // namespace SCS
+#endif // !_SCS_ALGORITMS_H
